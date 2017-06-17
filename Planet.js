4a86b3cae42c501e187dd9ffd95b5c2dc52a964a
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2017-06-16 23:00:27 -0300

    created planet object

--- Planet.js
+++ Planet.js
@@ -1,16 +1,15 @@
 /**
  * Created by Ignacio on 16/6/2017.
  */
-function Planet(mass, radius, xPostion, yPosition){
-    this.m
-}
-function newPlanet(xPosition,yPosition, radius){
+function Planet(mass, radius, xPosition, yPosition){
+    this.mass=mass;
+    this.radius=radius;
     var geometry = new THREE.SphereGeometry(radius,10,10);
     var material = new THREE.MeshLambertMaterial( {
         color: 0x00ff00} );
-    var sphere = new THREE.Mesh( geometry, material );
-    sphere.position.set(xPosition,yPosition,-500);
-    sphere.castShadow=true;
-    sphere.receiveShadow=true;
-    return sphere;
+    this.sphere = new THREE.Mesh( geometry, material );
+    this.sphere.castShadow=true;
+    this.sphere.receiveShadow=true;
+    this.sphere.position.set(xPosition,yPosition,-500);
 }
+
